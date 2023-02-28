ed80ee4f9a5de8746dfd0d34e60b336dad8eb704
unisanta <unisanta@M312-05>
2023-02-27 21:51:10 -0300

    Add project files.

---
+++ TODO-LIST/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace TODO_LIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblTarefa = new System.Windows.Forms.Label();
+            this.dataPicker = new System.Windows.Forms.DateTimePicker();
+            this.txtTarefa = new System.Windows.Forms.TextBox();
+            this.btnIncluir = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.lista = new System.Windows.Forms.ListBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(121, 87);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(30, 13);
+            this.lblData.TabIndex = 0;
+            this.lblData.Text = "Data";
+            // 
+            // lblTarefa
+            // 
+            this.lblTarefa.AutoSize = true;
+            this.lblTarefa.Location = new System.Drawing.Point(121, 155);
+            this.lblTarefa.Name = "lblTarefa";
+            this.lblTarefa.Size = new System.Drawing.Size(38, 13);
+            this.lblTarefa.TabIndex = 1;
+            this.lblTarefa.Text = "Tarefa";
+            // 
+            // dataPicker
+            // 
+            this.dataPicker.Location = new System.Drawing.Point(246, 79);
+            this.dataPicker.Name = "dataPicker";
+            this.dataPicker.Size = new System.Drawing.Size(200, 20);
+            this.dataPicker.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.dataPicker, "Insira a data da tarefa");
+            // 
+            // txtTarefa
+            // 
+            this.txtTarefa.Location = new System.Drawing.Point(246, 155);
+            this.txtTarefa.Name = "txtTarefa";
+            this.txtTarefa.Size = new System.Drawing.Size(200, 20);
+            this.txtTarefa.TabIndex = 3;
+            // 
+            // btnIncluir
+            // 
+            this.btnIncluir.Location = new System.Drawing.Point(479, 122);
+            this.btnIncluir.Name = "btnIncluir";
+            this.btnIncluir.Size = new System.Drawing.Size(75, 23);
+            this.btnIncluir.TabIndex = 4;
+            this.btnIncluir.Text = "Incluir";
+            this.btnIncluir.UseVisualStyleBackColor = true;
+            this.btnIncluir.Click += new System.EventHandler(this.btnIncluir_Click);
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(479, 372);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 23);
+            this.btnRemover.TabIndex = 5;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            this.btnRemover.Click += new System.EventHandler(this.btnRemover_Click);
+            // 
+            // lista
+            // 
+            this.lista.FormattingEnabled = true;
+            this.lista.Location = new System.Drawing.Point(124, 208);
+            this.lista.Name = "lista";
+            this.lista.Size = new System.Drawing.Size(322, 147);
+            this.lista.TabIndex = 6;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lista);
+            this.Controls.Add(this.btnRemover);
+            this.Controls.Add(this.btnIncluir);
+            this.Controls.Add(this.txtTarefa);
+            this.Controls.Add(this.dataPicker);
+            this.Controls.Add(this.lblTarefa);
+            this.Controls.Add(this.lblData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblTarefa;
+        private System.Windows.Forms.DateTimePicker dataPicker;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.TextBox txtTarefa;
+        private System.Windows.Forms.Button btnIncluir;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.ListBox lista;
+    }
+}
+
---
+++ TODO-LIST/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TODO_LIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIncluir_Click(object sender, EventArgs e)
+        {
+            if (txtTarefa.Text == "")
+            {
+                MessageBox.Show("Insira uma tarefa");
+            } else
+            {
+                lista.Items.Add($"{dataPicker.Value:d} - {txtTarefa.Text}");
+            }
+            
+        }
+
+        private void btnRemover_Click(object sender, EventArgs e)
+        {
+            lista.Items.Remove(lista.SelectedItem);
+        }
+    }
+}
